cf017c787bcb882d1f5aece36acd5300b4cc9ff4
John Nickell <john.r.nickell@gmail.com>
2026-04-18 17:53:40 -0500

    data types

---
+++ src/Adapter/Doctrine/StringObjectDataType.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fight\Common\Adapter\Doctrine;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\Exception\InvalidType;
+use Doctrine\DBAL\Types\Exception\ValueNotConvertible;
+use Doctrine\DBAL\Types\Type;
+use Fight\Common\Domain\Value\Basic\StringObject;
+use Throwable;
+
+/**
+ * Class StringObjectDataType
+ */
+final class StringObjectDataType extends Type
+{
+    public const TYPE_NAME = 'common_string';
+
+    /**
+     * Gets the SQL declaration snippet for a field of this type
+     */
+    public function getSQLDeclaration(array $column, AbstractPlatform $platform): string
+    {
+        return $platform->getStringTypeDeclarationSQL($column);
+    }
+
+    /**
+     * Converts a value from its PHP representation to its database representation
+     *
+     * @throws ConversionException When the conversion fails
+     */
+    public function convertToDatabaseValue(mixed $value, AbstractPlatform $platform): ?string
+    {
+        if (empty($value)) {
+            return null;
+        }
+
+        if (!($value instanceof StringObject)) {
+            throw InvalidType::new($value, 'string', [StringObject::class]);
+        }
+
+        return $value->toString();
+    }
+
+    /**
+     * Converts a value from its database representation to its PHP representation
+     *
+     * @throws ConversionException When the conversion fails
+     */
+    public function convertToPHPValue(mixed $value, AbstractPlatform $platform): ?StringObject
+    {
+        if ($value === null) {
+            return null;
+        }
+
+        if ($value instanceof StringObject) {
+            return $value;
+        }
+
+        try {
+            return StringObject::fromString($value);
+        } catch (Throwable $e) {
+            throw ValueNotConvertible::new($value, StringObject::class, $e->getMessage(), $e);
+        }
+    }
+
+    /**
+     * Gets the name of this type
+     */
+    public function getName(): string
+    {
+        return static::TYPE_NAME;
+    }
+}
---
+++ src/Adapter/Doctrine/UriDataType.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fight\Common\Adapter\Doctrine;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\Exception\InvalidType;
+use Doctrine\DBAL\Types\Exception\ValueNotConvertible;
+use Doctrine\DBAL\Types\Type;
+use Fight\Common\Domain\Value\Internet\Uri;
+use Throwable;
+
+/**
+ * Class UriDataType
+ */
+final class UriDataType extends Type
+{
+    public const TYPE_NAME = 'common_uri';
+
+    /**
+     * Gets the SQL declaration snippet for a field of this type
+     */
+    public function getSQLDeclaration(array $column, AbstractPlatform $platform): string
+    {
+        return $platform->getStringTypeDeclarationSQL($column);
+    }
+
+    /**
+     * Converts a value from its PHP representation to its database representation
+     *
+     * @throws ConversionException When the conversion fails
+     */
+    public function convertToDatabaseValue(mixed $value, AbstractPlatform $platform): ?string
+    {
+        if (empty($value)) {
+            return null;
+        }
+
+        if (!($value instanceof Uri)) {
+            throw InvalidType::new($value, 'string', [Uri::class]);
+        }
+
+        return $value->toString();
+    }
+
+    /**
+     * Converts a value from its database representation to its PHP representation
+     *
+     * @throws ConversionException When the conversion fails
+     */
+    public function convertToPHPValue(mixed $value, AbstractPlatform $platform): ?Uri
+    {
+        if (empty($value)) {
+            return null;
+        }
+
+        if ($value instanceof Uri) {
+            return $value;
+        }
+
+        try {
+            return Uri::fromString($value);
+        } catch (Throwable $e) {
+            throw ValueNotConvertible::new($value, Uri::class, $e->getMessage(), $e);
+        }
+    }
+
+    /**
+     * Gets the name of this type
+     */
+    public function getName(): string
+    {
+        return static::TYPE_NAME;
+    }
+}
