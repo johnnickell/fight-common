d5d80daf572e7917ed6e4c00defcde98b1fd71e6
John Nickell <john.r.nickell@gmail.com>
2026-04-22 15:35:29 -0500

    fixed command failed event

--- src/Domain/Messaging/Event/CommandFailedEvent.php
+++ src/Domain/Messaging/Event/CommandFailedEvent.php
@@ -4,18 +4,52 @@ declare(strict_types=1);
 
 namespace Fight\Common\Domain\Messaging\Event;
 
+use Fight\Common\Domain\Exception\DomainException;
 use Fight\Common\Domain\Messaging\Command\Command;
 
 /**
  * Class CommandFailedEvent
  */
-class CommandFailedEvent implements Event
+final readonly class CommandFailedEvent implements Event
 {
     /**
      * Constructs CommandFailedEvent
      */
     public function __construct(private readonly Command $command, private readonly string $errorMessage)
     {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function fromArray(array $data): static
+    {
+        $keys = ['command_class', 'command_data', 'error_message'];
+
+        foreach ($keys as $key) {
+            if (!array_key_exists($key, $data)) {
+                $message = sprintf('Missing required key "%s" in data array', $key);
+                throw new DomainException($message);
+            }
+        }
+
+        $commandClass = $data['command_class'];
+        $command = $commandClass::fromArray($data['command_data']);
+        $errorMessage = $data['error_message'];
+
+        return new static($command, $errorMessage);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function toArray(): array
+    {
+        return [
+            'command_class' => $this->command::class,
+            'command_data'  => $this->command->toArray(),
+            'error_message' => $this->errorMessage
+        ];
     }
 
     /**
