111602c17d621347b00c9f34affe792bfdb67301
John Nickell <john.r.nickell@gmail.com>
2026-04-27 16:20:23 -0500

    validation attribute subscriber

---
+++ src/Adapter/EventSubscriber/SymfonyValidationSubscriber.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fight\Common\Adapter\EventSubscriber;
+
+use Fight\Common\Application\Attribute\Validation;
+use Fight\Common\Application\Validation\ValidationService;
+use ReflectionClass;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Throwable;
+
+/**
+ * Class SymfonyValidationSubscriber
+ */
+final readonly class SymfonyValidationSubscriber implements EventSubscriberInterface
+{
+    /**
+     * Constructs SymfonyValidationSubscriber
+     */
+    public function __construct(private ValidationService $validationService)
+    {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [KernelEvents::CONTROLLER => 'validateControllerInput'];
+    }
+
+    /**
+     * Checks Validation Attribute on controller methods
+     *
+     * @throws Throwable
+     */
+    public function validateControllerInput(ControllerEvent $event): void
+    {
+        $controller = $event->getController();
+        $request = $event->getRequest();
+
+        if (!is_array($controller) && method_exists($controller, '__invoke')) {
+            $controller = [$controller, '__invoke'];
+        }
+
+        if (!is_array($controller) || !is_object($controller[0])) {
+            return;
+        }
+
+        $className = get_class($controller[0]);
+        $reflection = new ReflectionClass($className);
+        $reflectionMethod = $reflection->getMethod($controller[1]);
+        $attributes = $reflectionMethod->getAttributes(Validation::class);
+        foreach ($attributes as $attribute) {
+            /** @var Validation $validation */
+            $validation = $attribute->newInstance();
+            $inputData = $request->isMethodSafe() ? $request->query->all() : $request->request->all();
+            $this->validationService->validate($inputData, $validation->rules());
+        }
+    }
+}
