99c4537e9635e59b9692426c39af9981daffa1ad
John Nickell <john.r.nickell@gmail.com>
2026-05-08 15:53:59 -0500

    unit tests

--- tests/Application/HttpClient/Exception/RequestExceptionTest.php
+++ tests/Application/HttpClient/Exception/RequestExceptionTest.php
@@ -7,6 +7,7 @@ namespace Fight\Test\Common\Application\HttpClient\Exception;
 use Fight\Common\Application\HttpClient\Exception\RequestException;
 use Fight\Common\Application\HttpClient\Exception\TransferException;
 use Fight\Test\Common\TestCase\UnitTestCase;
+use Mockery\MockInterface;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Psr\Http\Message\RequestInterface;
 
@@ -15,6 +16,7 @@ class RequestExceptionTest extends UnitTestCase
 {
     public function test_that_construction_with_message_and_request_sets_message(): void
     {
+        /** @var MockInterface|RequestInterface $request */
         $request = $this->mock(RequestInterface::class);
 
         $exception = new RequestException('Something went wrong', $request);
@@ -24,6 +26,7 @@ class RequestExceptionTest extends UnitTestCase
 
     public function test_that_get_request_returns_the_request_passed_to_constructor(): void
     {
+        /** @var MockInterface|RequestInterface $request */
         $request = $this->mock(RequestInterface::class);
 
         $exception = new RequestException('Error', $request);
@@ -33,6 +36,7 @@ class RequestExceptionTest extends UnitTestCase
 
     public function test_that_request_exception_extends_transfer_exception(): void
     {
+        /** @var MockInterface|RequestInterface $request */
         $request = $this->mock(RequestInterface::class);
 
         $exception = new RequestException('Error', $request);
